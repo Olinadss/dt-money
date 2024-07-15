4495b1228e3c5f954c0b31f8d48f3b0e72e0e4e9
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-07-15 17:26:18 -0300

    feat: cria estrutura do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
 
 export function Header() {
@@ -7,7 +9,20 @@ export function Header() {
 		<HeaderContainer>
 			<HeaderContent>
 				<img src={logoImg} alt='' />
-				<NewTransactionButton>Nova transação</NewTransactionButton>
+
+				<Dialog.Root>
+					<Dialog.Trigger asChild>
+						<NewTransactionButton>Nova transação</NewTransactionButton>
+					</Dialog.Trigger>
+					<Dialog.Portal>
+						<Dialog.Overlay className='overlay' />
+						<Dialog.Content>
+							<Dialog.Title>Nova Transação</Dialog.Title>
+
+							<Dialog.Close />
+						</Dialog.Content>
+					</Dialog.Portal>
+				</Dialog.Root>
 			</HeaderContent>
 		</HeaderContainer>
 	)
