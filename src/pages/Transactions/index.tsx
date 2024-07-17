f3d31c13da48b71b6604cd65e36a1cbffa4ba763
Danilo dos Santos <danilo.souza@brisabr.com.br>
2024-07-17 11:06:36 -0300

    feat: adiciona a formatação de valores monetários

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
 	TransactionsTable,
 } from './styles'
 import { TransactionContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
 	const { transactions } = useContext(TransactionContext)
@@ -25,11 +26,12 @@ export function Transactions() {
 								<td width={'50%'}>{transaction.description}</td>
 								<td>
 									<PriceHighLight variant={transaction.type}>
-										{transaction.price}
+										{transaction.type === 'outcome' && '- '}
+										{priceFormatter.format(transaction.price)}
 									</PriceHighLight>
 								</td>
 								<td>{transaction.category}</td>
-								<td>{transaction.createdAt}</td>
+								<td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
 							</tr>
 						))}
 					</tbody>
