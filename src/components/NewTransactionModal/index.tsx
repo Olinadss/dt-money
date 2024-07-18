1368ed675cd6970c916caf00f25b1163c6f732ca
Danilo dos Santos <danilo.souza@brisabr.com.br>
2024-07-18 15:59:15 -0300

    feat: adiciona react hook forms e zod nos formularios

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,7 +8,34 @@ import {
 } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newTransactionFormSchema = zod.object({
+	description: zod.string(),
+	price: zod.number(),
+	category: zod.string(),
+	type: zod.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>
+
 export function NewTransactionModal() {
+	const {
+		register,
+		handleSubmit,
+		formState: { isSubmitting },
+	} = useForm<NewTransactionFormInputs>({
+		resolver: zodResolver(newTransactionFormSchema),
+	})
+
+	async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+		await new Promise(resolve => setTimeout(resolve, 2000))
+
+		console.log(data)
+	}
+
 	return (
 		<Dialog.Portal>
 			<Overlay className='overlay' />
@@ -19,10 +46,25 @@ export function NewTransactionModal() {
 					<X size={24} />
 				</CloseButton>
 
-				<form action=''>
-					<input type='text' placeholder='Descrição' required />
-					<input type='number' placeholder='Preço' required />
-					<input type='text' placeholder='Categoria' required />
+				<form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+					<input
+						type='text'
+						placeholder='Descrição'
+						required
+						{...register('description')}
+					/>
+					<input
+						type='number'
+						placeholder='Preço'
+						required
+						{...register('price', { valueAsNumber: true })}
+					/>
+					<input
+						type='text'
+						placeholder='Categoria'
+						required
+						{...register('category')}
+					/>
 
 					<TransactionType>
 						<TransactionTypeButton
@@ -41,7 +83,9 @@ export function NewTransactionModal() {
 						</TransactionTypeButton>
 					</TransactionType>
 
-					<button type='submit'>Cadastrar</button>
+					<button type='submit' disabled={isSubmitting}>
+						Cadastrar
+					</button>
 				</form>
 			</Content>
 		</Dialog.Portal>
